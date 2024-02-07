1af2336d07128b7c6964c23bfe9c677d66161995
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-07 23:12:12 +0100

    Add the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import DisplayQuiz from '@/components/quiz/DisplayQuiz.vue';
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <main>
-    <DisplayQuiz/>
-  </main>
+  <div>
+    <h2>Welcome to Quizzy</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id cupiditate animi quisquam molestiae eum, itaque pariatur quaerat, illo voluptatibus sint velit possimus odio enim nisi sit. Provident, cumque non?
+    </p>
+    <router-link to="/question/1">
+      Start Quiz
+    </router-link>
+  </div>
 </template>
