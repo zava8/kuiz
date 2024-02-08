12c1d293a3b03e800a9a32f417c05983c7693a19
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-08 12:15:36 +0100

    Add a descriptive welcome message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,39 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <div>
-    <h2>Welcome to Quizzy</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id cupiditate animi quisquam molestiae eum, itaque pariatur quaerat, illo voluptatibus sint velit possimus odio enim nisi sit. Provident, cumque non?
-    </p>
-    <router-link to="/question/1">
-      Start Quiz
-    </router-link>
+  <div class="container">
+    <div>
+      <h2>Welcome to Quizzy</h2>
+      <p>
+        This is a demo quiz app bulit with VueJS
+      </p>
+    </div>
+    <div>
+      <router-link to="/question/1" class="start">
+        Start Quiz
+      </router-link>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .container {
+    display: grid;
+    gap: 1em;
+  }
+
+  p {
+    width: 40%;
+    color: white;
+    margin: 0 auto;
+  }
+  .start {
+    display: inline-block;
+    background-color: skyblue;
+    padding: 0.6em;
+    text-decoration: none;
+    font-weight: bold;
+    color: black;
+    border-radius: 0.6px;
+  }
+</style>
